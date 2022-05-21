82018c4200558373c20ebb4e532ae78d3ee80909
Zheng Tang <oscartang1120@gmail.com>
2022-05-20 18:24:27 -0700

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -58,6 +58,7 @@ class EpisodeDataset(Dataset):
                     words.append(sub_token)
         
         words = ['[CLS]'] + words + ['[SEP]']
+        print (words)
         tokens = self.tokenizer.convert_tokens_to_ids(words)
         # if len(tokens) > self.opt['max_length']:
         #     tokens = tokens[:self.opt['max_length']]
