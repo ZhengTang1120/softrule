40ef007cecdaa360426be39b3884eee8f93f571b
Zheng Tang <oscartang1120@gmail.com>
2022-05-20 17:41:39 -0700

    Update models.py

--- models.py
+++ models.py
@@ -12,6 +12,7 @@ class BertEM(nn.Module):
 
     def forward(self, words):
         output = self.model(words)
+        h = output.last_hidden_state
         subj_mask = torch.logical_and(words.unsqueeze(2).gt(0), words.unsqueeze(2).lt(3))
         obj_mask = torch.logical_and(words.unsqueeze(2).gt(2), words.unsqueeze(2).lt(20))
         v = torch.cat([pool(h, subj_mask.eq(0), type=pool_type), pool(h, obj_mask.eq(0), type=pool_type)], 1)
