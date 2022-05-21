ed2e64af53da98a01a27fde245d6d73ba4d98ed7
Zheng Tang <oscartang1120@gmail.com>
2022-05-20 18:02:57 -0700

    Update models.py

--- models.py
+++ models.py
@@ -11,10 +11,13 @@ class BertEM(nn.Module):
             self.nav = Variable(torch.randn(m, in_dim)).cuda()
 
     def forward(self, words):
+        print (words)
         output = self.model(words)
         h = output.last_hidden_state
         subj_mask = torch.logical_and(words.unsqueeze(2).gt(0), words.unsqueeze(2).lt(3))
         obj_mask = torch.logical_and(words.unsqueeze(2).gt(2), words.unsqueeze(2).lt(20))
+        print (subj_mask)
+        print (obj_mask)
         v = torch.cat([pool(h, subj_mask.eq(0), type='avg'), pool(h, obj_mask.eq(0), type='avg')], 1)
         return v
 
