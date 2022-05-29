9219eade78e339cfe16e5b70d58b1e0269f4ae18
Zheng Tang <oscartang1120@gmail.com>
2022-05-29 11:29:43 -0700

    Update train.py

--- train.py
+++ train.py
@@ -34,7 +34,8 @@ parser.add_argument("--eval_per_epoch", default=10, type=int, help="How many tim
 parser.add_argument('--bert', default='bert-base-uncased', type=str, help='Which bert to use.')
 parser.add_argument('--train', default='train_episode.json', type=str, help='Which training set to use.')
 parser.add_argument('--seed', default=42, type=int, help='Random Seed')
-parser.add_argument('--hidden_dim', default=2000, type=int, help='Hidden Size')
+parser.add_argument('--hidden_dim', default=2000, type=int)
+parser.add_argument('--instances_per_epoch', default=700, type=int)
 
 args = parser.parse_args()
 opt = vars(args)
@@ -44,8 +45,8 @@ random.seed(args.seed)
 
 tokenizer = BertTokenizer.from_pretrained(opt['bert'])
 train_set = EpisodeDataset(opt['data_dir']+opt['train'], tokenizer)
-randsampler = RandomSampler(train_set, num_samples=700)
-train_batches_size = 700 // opt['batch_size']
+randsampler = RandomSampler(train_set, num_samples=opt['instances_per_epoch'])
+train_batches_size = opt['instances_per_epoch'] // opt['batch_size']
 dev_set = EpisodeDataset(opt['data_dir']+'dev_episode.json', tokenizer)
 dev_batches = DataLoader(dev_set, batch_size=1, collate_fn=collate_batch)
 opt['num_training_steps'] = train_batches_size * opt['num_epoch']
