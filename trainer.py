341b82bd6eecf1eb8522b2b811289e136ffdd56a
Zheng Tang <oscartang1120@gmail.com>
2022-05-30 00:19:27 -0700

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -61,8 +61,7 @@ def unpack_batch(batch, cuda=False, device=0):
     batch_size = support_sents.size(0)
     N = support_sents.size(1)
     k = support_sents.size(2)
-    query_size = query.size(0) / batch_size
-    print (query_size)
+    query_size = query.size(0) // batch_size
     return query, support_sents, labels, N, k, batch_size, query_size
 
 
