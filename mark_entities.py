328da8b530ebb766488b3e06a7372903f14be609
Zheng Tang <oscartang1120@gmail.com>
2022-06-03 03:37:09 -0700

    Update mark_entities.py

--- mark_entities.py
+++ mark_entities.py
@@ -3,6 +3,11 @@ from preprocessing_prepeare_sentence import preprocessing
 
 model_name = 'bert-base-cased'
 pre = preprocessing(model_name)
+
+f = json.load(open("dev_episode.json"))
+
+episodes = f[0]
+
 for i, ep in enumerate(episodes):
     for j, q in enumerate(ep['meta_test']):
         sentence_info = pre.preprocessing_flow(copy.deepcopy(q))
