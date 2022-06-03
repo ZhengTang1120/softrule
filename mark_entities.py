e5e9e6b4554649cc1531c1560d97220319e21e17
Zheng Tang <oscartang1120@gmail.com>
2022-06-03 04:00:52 -0700

    Update mark_entities.py

--- mark_entities.py
+++ mark_entities.py
@@ -11,7 +11,11 @@ f = json.load(open("dev_episode.json"))
 
 episodes = f[0]
 
+new = [[], f[1]]
+
 for i, ep in enumerate(episodes):
+    new[0].append({'meta_test':[]})
+    new[0].append({'meta_train':[]})
     for j, q in enumerate(ep['meta_test']):
         sentence_info = pre.preprocessing_flow(copy.deepcopy(q))
         tokens_with_markers, h_start, t_start, h_end, t_end = sentence_info
@@ -20,10 +24,13 @@ for i, ep in enumerate(episodes):
         ep['meta_test'][j]["tokens_with_markers"] = tokens_with_markers
         ep['meta_test'][j]["head_end"] = h_end
         ep['meta_test'][j]["tail_end"] = t_end
-        for key in ep['meta_test'][j]:
-            if key not in important_keys:
-                ep['meta_test'][j].pop(key, None)
+
+        new_instance = {k:v for k, v in ep['meta_test'][j].items() if k in important_keys}
+
+        new[0][-1]['meta_test'].append(new_instance)
+
     for j, way in enumerate(ep['meta_train']):
+        new[0][-1]['meta_train'].append([])
         for k, shot in enumerate(way):
             sentence_info = pre.preprocessing_flow(copy.deepcopy(shot))
             tokens_with_markers, h_start, t_start, h_end, t_end = sentence_info
@@ -32,8 +39,10 @@ for i, ep in enumerate(episodes):
             ep['meta_train'][j][k]["tokens_with_markers"] = tokens_with_markers
             ep['meta_train'][j][k]["head_end"] = h_end
             ep['meta_train'][j][k]["tail_end"] = t_end
-            for key in ep['meta_train'][j][k]:
-                if key not in important_keys:
-                    ep['meta_train'][j][k].pop(key, None)
-print (json.dumps(f))
+
+            new_instance = {k:v for k, v in ep['meta_train'][j][k].items() if k in important_keys}
+
+            new[0][-1]['meta_train'][-1].append(new_instance)
+
+print (json.dumps(new))
             
