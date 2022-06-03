c93c7b3f5d722abd963db770f3d9f82c513358df
Zheng Tang <oscartang1120@gmail.com>
2022-06-03 03:44:46 -0700

    Update mark_entities.py

--- mark_entities.py
+++ mark_entities.py
@@ -13,9 +13,19 @@ episodes = f[0]
 for i, ep in enumerate(episodes):
     for j, q in enumerate(ep['meta_test']):
         sentence_info = pre.preprocessing_flow(copy.deepcopy(q))
-        print (sentence_info)
+        q["head_after_bert"] = h_start
+        q["tail_after_bert"] = t_start
+        q["tokens_with_markers"] = tokens_with_markers
+        q["head_end"] = h_end
+        q["tail_end"] = t_end
     for way in ep['meta_train']:
         for shot in way:
             sentence_info = pre.preprocessing_flow(copy.deepcopy(shot))
-            print (sentence_info)
+            q["head_after_bert"] = h_start
+            q["tail_after_bert"] = t_start
+            q["tokens_with_markers"] = tokens_with_markers
+            q["head_end"] = h_end
+            q["tail_end"] = t_end
+
+print (json.dumps(f))
             
