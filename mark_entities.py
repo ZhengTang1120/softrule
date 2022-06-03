253cd9e79feecdcf386a6174fa7035b622584889
Zheng Tang <oscartang1120@gmail.com>
2022-06-03 03:49:15 -0700

    Update mark_entities.py

--- mark_entities.py
+++ mark_entities.py
@@ -14,20 +14,20 @@ for i, ep in enumerate(episodes):
     for j, q in enumerate(ep['meta_test']):
         sentence_info = pre.preprocessing_flow(copy.deepcopy(q))
         tokens_with_markers, h_start, t_start, h_end, t_end = sentence_info
-        q["head_after_bert"] = h_start
-        q["tail_after_bert"] = t_start
-        q["tokens_with_markers"] = tokens_with_markers
-        q["head_end"] = h_end
-        q["tail_end"] = t_end
-    for way in ep['meta_train']:
-        for shot in way:
+        ep['meta_test'][j]["head_after_bert"] = h_start
+        ep['meta_test'][j]["tail_after_bert"] = t_start
+        ep['meta_test'][j]["tokens_with_markers"] = tokens_with_markers
+        ep['meta_test'][j]["head_end"] = h_end
+        ep['meta_test'][j]["tail_end"] = t_end
+    for j, way in enumerate(ep['meta_train']):
+        for k, shot in enumerate(way):
             sentence_info = pre.preprocessing_flow(copy.deepcopy(shot))
             tokens_with_markers, h_start, t_start, h_end, t_end = sentence_info
-            q["head_after_bert"] = h_start
-            q["tail_after_bert"] = t_start
-            q["tokens_with_markers"] = tokens_with_markers
-            q["head_end"] = h_end
-            q["tail_end"] = t_end
+            ep['meta_train'][j][k]["head_after_bert"] = h_start
+            ep['meta_train'][j][k]["tail_after_bert"] = t_start
+            ep['meta_train'][j][k]["tokens_with_markers"] = tokens_with_markers
+            ep['meta_train'][j][k]["head_end"] = h_end
+            ep['meta_train'][j][k]["tail_end"] = t_end
     print (ep)
     exit()
 # print (json.dumps(f))
