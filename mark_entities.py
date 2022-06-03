4c17d799465583156205ee333fda423cd5711cf6
Zheng Tang <oscartang1120@gmail.com>
2022-06-03 04:13:25 -0700

    Update mark_entities.py

--- mark_entities.py
+++ mark_entities.py
@@ -7,7 +7,7 @@ model_name = 'bert-base-cased'
 pre = preprocessing(model_name)
 important_keys = set(['id', 'relation', 'token', 'tokens', 'h', 't', 'head_after_bert', 'tail_after_bert', 'tokens_with_markers', 'head_end', 'tail_end'])
 
-f = json.load(open("dev_episode.json"))
+f = json.load(open("5_way_1_shots_10K_episodes_3q_seed_160290.json"))
 
 episodes = f[0]
 
