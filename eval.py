20bbf2f978e5a308f0f42bc86a04ff100a1b0845
Zheng Tang <oscartang1120@gmail.com>
2022-06-14 18:03:18 -0700

    Update eval.py

--- eval.py
+++ eval.py
@@ -14,7 +14,7 @@ def load_config(filename):
     return dump['config']
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--model_dir', type=str, default='./')
+parser.add_argument('--model_dir', type=str, default='models')
 parser.add_argument('--dataset', type=str, default='test', help="Evaluate on dev or test.")
 parser.add_argument('--device', type=int, default=0, help='gpu device to use.')
 args = parser.parse_args()
@@ -30,29 +30,27 @@ trainer.load(model_file)
 
 tokenizer = BertTokenizer.from_pretrained(opt['bert'])
 
-data_set = EpisodeDataset(opt['data_dir']+f'{args.dataset}', tokenizer)
-data_batches = DataLoader(data_set, batch_size=1, collate_fn=collate_batch)
+with open('categories_split.json') as f:
+    splits = json.load(f)
+
+data_set = EpisodeDataset(f'{args.dataset}', tokenizer)
+data_batches = DataLoader(data_set, batch_size=1, collate_fn=collate_batch, splits['test'])
 
 preds = []
 golds = []
 for db in data_batches:
     score, loss, labels = trainer.predict(db)
-    preds += np.argmax(score.squeeze(2).data.cpu().numpy(), axis=1).tolist()
-    golds += labels.cpu().tolist()
+    preds += np.around(score.view(-1).data.cpu().numpy()).tolist()
+    golds += labels.view(-1).cpu().tolist()
 
-nrp = [0 if p >= 5 else 1 for p in preds]
-nrg = [0 if g >= 5 else 1 for g in golds]
-
-matched = [1 if p == nrg[i] and p == 1 else 0 for i, p in enumerate(nrp)]
-
-print (sum(nrp), sum(nrg))
+matched = [1 if p == golds[j] and p == 1 else 0 for j, p in enumerate(preds)]
 
 try:
-    recall = sum(matched)/sum(nrg)
+    recall = sum(matched)/sum(golds)
 except ZeroDivisionError:
     recall = 0
 try:
-    precision = sum(matched)/sum(nrp)
+    precision = sum(matched)/sum(preds)
 except ZeroDivisionError:
     precision = 0
 try:
@@ -61,8 +59,8 @@ except ZeroDivisionError:
     f1 = 0
 
 with open("NRC_output_%s.txt"%args.dataset.split('.')[0], 'w') as f:
-    for i in range(0, len(nrp), data_set.query_size):
-        output = ["no_relation" if p == 0 else "relation" for p in nrp[i:i+data_set.query_size]]
+    for i in range(0, len(preds), data_set.query_size):
+        output = ["no_relation" if p == 0 else "relation" for p in preds[i:i+data_set.query_size]]
         f.write("\t".join(output))
         f.write('\n')
 
